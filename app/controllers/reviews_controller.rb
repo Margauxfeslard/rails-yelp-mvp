1ae0c9f78171420e2e4f9d84ff1b5a075ee3e9e4
Margaux FESLARD margaux.feslard@gmail.com <margaux.feslard@gmail.com>
2019-05-22 15:21:39 +0200

    restaurant

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params_review)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'La Belleza',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'belgian',
+  },
+  {
+    name:         'La Fleur de Lille',
+    address:      '1 rue ratisbonne 59000 Lille',
+    category:  'japanese',
+  },
+  {
+    name:         'Dishoom',
+    address:      '4 rue bethune 59000 Lille',
+    category:  'chinese',
+  },
+  {
+    name:         'La famille',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+  },
+  {
+    name:         '3 brasseurs',
+    address:      '3 rue maginot 59000 Lille',
+    category:  'french',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
